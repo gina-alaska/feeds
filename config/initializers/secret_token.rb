85ac26e081d517a4cb2a4e4087fdf3d15a8b1bbe
Will Fisher <will@teknofire.net>
2012-07-03 14:33:03 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feeds::Application.config.secret_token = '622e949f2ff0510df898407e9effaa1993eed08d430923003be3b8f92dfdaa606c6086a90ec0621974c907e86cd95b2829bf3d552ff418a490a3535e7cc1c472'
