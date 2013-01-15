c4a16b91fb6cdc3d11c5c9a7e6da1a5ae1eecff8
Will Fisher <will@teknofire.net>
2013-01-15 11:01:20 -0900

    fixing indexes

--- app/models/realtime_tile.rb
+++ app/models/realtime_tile.rb
@@ -4,8 +4,8 @@ class RealtimeTile
   field :type,        type: String
   field :properties,  type: Hash
   field :geometry,    type: Hash
-  index :type
-  index 'properties.id'
+  index :type => 1
+  index 'properties.id' => 1
 
   def as_json(*args)
     data = super
